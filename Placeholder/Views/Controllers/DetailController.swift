d5611eaf0ff2586ad797dc7b6c73a6ebdb4b4964
Hugo Flores <hfp.ing2@gmail.com>
2020-04-05 10:42:47 -0400

    Detail View

--- Placeholder/Views/Controllers/DetailController.swift
+++ Placeholder/Views/Controllers/DetailController.swift
@@ -9,22 +9,66 @@
 import UIKit
 
 class DetailController: UIViewController {
+    
+    let viewModel = DetailsViewModel()
+    
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
+    
+    let scrollView: UIScrollView = {
+        let sv = UIScrollView()
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        return sv
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrollView.delegate = self
+        print("Method set?")
+        viewModel.onImageSet = setImage
 
-        // Do any additional setup after loading the view.
+        setUpMain()
+        setUpPinchToZoom()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUpMain() {
+        scrollView.addSubview(imageView)
+        view.addSubview(scrollView)
+        
+        NSLayoutConstraint.activate([
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            imageView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            imageView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            imageView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            
+            imageView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            imageView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
+        ])
     }
-    */
+    
+    func setUpPinchToZoom() {
+        scrollView.maximumZoomScale = 4
+        scrollView.minimumZoomScale = 1
+    }
+    
+    func setImage() {
+        imageView.image = UIImage(data: viewModel.image!)
+    }
 
+}
+
+extension DetailController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
 }
